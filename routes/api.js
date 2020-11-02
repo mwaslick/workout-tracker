5eff82a822bae0c6d79cece09dbba29f50d41efb
Maria Waslick <mariawaslick@gmail.com>
2020-11-02 12:25:41 -0500

    got post requests working

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,40 @@
 const router = require("express").Router();
-const { db } = require("../models/Exercise.js");
-const Exercise = require("../models/Exercise.js");
-const Workout = require("../models/Workout.js");
+const db = require("../models/")
+const mongoose = require("mongoose")
 
-app.post("/newworkout", ({body}, res) => {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { 
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false
+ });
+
+router.post("/newworkout", ({body}, res) => {
     db.Workout.create(body)
     .then(dbWorkout => {
         console.log(dbWorkout)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+router.post("/submit", ({body}, res) => {
+    db.Exercise.create(body)
+    .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: {reps: _id}}, {new: true}))
+    .then (dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    })
+})
+
+router.get("/populateworkout", (req, res) => {
+    db.Workout.find({})
+    .populate("reps")
+    .then(dbWorkout => {
+        res.json(dbWorkout)
     })
     .catch(err => {
         res.json(err)
